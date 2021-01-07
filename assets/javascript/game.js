f96dbee24808fdaefb2868752d7a6c5f9dda1cef
CaoKao <bksaechao@gmail.com>
2021-01-07 01:32:01 -0800

    created a function to overwrite html and display 'game over' when all the guessses are used up. still need to make the counter decrement only when a user guesses wrong

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,24 +2,42 @@ var mysteryWord = document.getElementById("mystery-word");
 var userGuesses = document.getElementById("user-guesses");
 var randomWord = "JUNGLE";
 var userGuessArray = [];
+var remainingGuesses = document.getElementById("remaining-guesses");
+
+let guessesLeft = randomWord.length;
+
+
+remainingGuesses.innerText = guessesLeft;
 
 function recordGuess() {
     userGuessArray.push(userInput);
     userGuesses.innerText = userGuessArray;
 }
 
+function updateScore() {
+    remainingGuesses.innerText = guessesLeft;
+}
+
+function gameOver() {
+    remainingGuesses.innerText = "Game Over!"
+}
+
 function addCorrectLetter() {
     for (let i = 0; i < randomWord.length; i++) {
         if (userInput === randomWord[i]) {
             mysteryWord.append(userInput);
-        } else { }
+        }
     }
 }
 
 document.onkeyup = function (event) {
     userInput = event.key.toUpperCase();
-    addCorrectLetter();
-    recordGuess();
+    guessesLeft--;
+    if (guessesLeft > 0) {
+        addCorrectLetter();
+        recordGuess();
+        updateScore();
+    } else {
+        gameOver();
+    }
 }
-
-// if randomWord.includes(userInput)
